3258c09b59d351871dbda6161c80b3c364d3b0da
joschonarth <joschonarth@gmail.com>
2026-06-19 12:55:10 -0300

    refactor: rename main class

--- src/main/java/br/com/joschonarth/springpay/SpringPayApplication.java
+++ src/main/java/br/com/joschonarth/springpay/SpringPayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringpayApplication {
+public class SpringPayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringpayApplication.class, args);
+		SpringApplication.run(SpringPayApplication.class, args);
 	}
 
 }
--- src/test/java/br/com/joschonarth/springpay/SpringPayApplicationTests.java
+++ src/test/java/br/com/joschonarth/springpay/SpringPayApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringpayApplicationTests {
+class SpringPayApplicationTests {
 
 	@Test
 	void contextLoads() {
